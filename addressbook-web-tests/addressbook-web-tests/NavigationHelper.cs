0f75431088f0ad4a1fe10b5c71c39efced82c0d2
Andrew77747 <andrew-walker@yandex.ru>
2023-09-21 18:50:20 +0300

    добавил хелперы

---
+++ addressbook-web-tests/addressbook-web-tests/NavigationHelper.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    public class NavigationHelper : HelperBase
+    {
+        private string baseUrl;
+
+        public NavigationHelper(IWebDriver driver, string baseUrl) : base(driver)
+        {
+            this.baseUrl = baseUrl;
+        }
+
+        public void GoToHomePage()
+        {
+            driver.Navigate().GoToUrl(baseUrl);
+        }
+
+        public void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/TestBase.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    public class TestBase
+    {
+        protected IWebDriver driver;
+        protected StringBuilder verificationErrors;
+        protected string baseURL;
+        protected LoginHelper loginHelper;
+        protected NavigationHelper navigator;
+        protected GroupHelper groupHelper;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            baseURL = "http://localhost/addressbook";
+            verificationErrors = new StringBuilder();
+
+            loginHelper = new LoginHelper(driver);
+            navigator = new NavigationHelper(driver, baseURL);
+            groupHelper = new GroupHelper(driver);
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+    }
+}
