c173563d060376e302f2983f39e5402bc589306a
Andrew77747 <andrew-walker@yandex.ru>
2023-09-22 19:19:52 +0300

    добавил ApplicationManager

--- addressbook-web-tests/addressbook-web-tests/AppManager/ApplicationManager.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/ApplicationManager.cs
@@ -1,6 +1,5 @@
-﻿using NUnit.Framework;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,30 +8,27 @@ using System.Threading.Tasks;
 
 namespace WebAddressbookTests
 {
-    public class TestBase
+    public class ApplicationManager
     {
         protected IWebDriver driver;
-        protected StringBuilder verificationErrors;
         protected string baseURL;
+
         protected LoginHelper loginHelper;
         protected NavigationHelper navigator;
         protected GroupHelper groupHelper;
 
-        [SetUp]
-        public void SetupTest()
+        public ApplicationManager()
         {
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             baseURL = "http://localhost/addressbook";
-            verificationErrors = new StringBuilder();
 
             loginHelper = new LoginHelper(driver);
             navigator = new NavigationHelper(driver, baseURL);
             groupHelper = new GroupHelper(driver);
         }
 
-        [TearDown]
-        public void TeardownTest()
+        public void Stop()
         {
             try
             {
@@ -42,7 +38,30 @@ namespace WebAddressbookTests
             {
                 // Ignore errors if unable to close the browser
             }
-            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        public LoginHelper Auth
+        {
+            get
+            {
+                return loginHelper;
+            }
+        }
+
+        public NavigationHelper Navigator
+        {
+            get
+            {
+                return navigator;
+            }
+        }
+
+        public GroupHelper Groups
+        {
+            get
+            {
+                return groupHelper;
+            }
         }
     }
 }
---
+++ addressbook-web-tests/addressbook-web-tests/Tests/TestBase.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    public class TestBase
+    {
+        protected ApplicationManager app;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            app = new ApplicationManager();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            app.Stop();
+        }
+    }
+}
